4964992fd8a2eabc673f6e24f6eb5de8832a0432
Zann <damiryusupov2504@gmail.com>
2023-10-31 17:30:06 +0500

    new

---
+++ prjkn/Form1.cs
@@ -0,0 +1,20 @@
+namespace prjkn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
