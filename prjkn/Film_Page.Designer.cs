b1e73cf02e644d7f469c2218aca3d450f01bab80
Zann <damiryusupov2504@gmail.com>
2023-11-01 10:16:33 +0500

    new Page

---
+++ prjkn/Film_Page.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace prjkn
+{
+    partial class Film_Page
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Film_Page));
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(792, 100);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(200, 407);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.Dock = DockStyle.Top;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(992, 100);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Dock = DockStyle.Left;
+            panel3.Location = new Point(0, 100);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(200, 407);
+            panel3.TabIndex = 2;
+            // 
+            // Film_Page
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(992, 507);
+            Controls.Add(panel3);
+            Controls.Add(panel1);
+            Controls.Add(panel2);
+            Name = "Film_Page";
+            Text = "Film_Page";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+    }
+}
---
+++ prjkn/Film_Page.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prjkn
+{
+    public partial class Film_Page : Form
+    {
+        public Film_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
